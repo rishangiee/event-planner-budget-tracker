81dbb69a2de47f9321e213cfe9d701b3749906ed
kakai <familypalmitos@gmail.com>
2026-04-29 00:31:25 +0800

    Delete event-planner-budget-tracker directory

--- event-planner-budget-tracker/backend/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",        # default XAMPP user
-        password="",        # default XAMPP password is empty
-        database="cavendia_db"
-    )
